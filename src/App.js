5fed8064bc436f85e2f5e911e6653c02edfc632b
Andres Marquez <andresdmf55@gmail.com>
2022-04-27 13:09:02 -0500

    Setting main files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme, GlobalStyles } from './components/theme';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    const [ theme, setTheme ] = useState('darkTheme');
+
+    const changeTheme = (change) => {
+        
+        if(!change){
+            setTheme('lightTheme');
+        } else{
+            setTheme('darkTheme');
+        }
+
+    }
+
+    return(
+        <ThemeProvider theme={theme === 'darkTheme' ? darkTheme : lightTheme} >
+            <GlobalStyles />
+                <Header themeValue={changeTheme} />
+                <Main />
+                <Footer />
+        </ThemeProvider>
+    );
 }
 
 export default App;
