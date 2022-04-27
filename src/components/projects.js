feea8e212579cf298498dba2ec93c404bd8a6226
Andres Marquez <andresdmf55@gmail.com>
2022-04-27 13:18:44 -0500

    Projects

---
+++ src/components/projects.js
@@ -0,0 +1,57 @@
+/**
+ * All projects I have done. I just add an image and a url per project.
+ */
+
+const projects = [
+    {
+        id: 1,
+        title: 'Binance Api',
+        img: 'img/apiBinance.jpg',
+        alt: 'binance-api',
+        link: 'https://andres-webdev.github.io/binance-api/',
+        category: 'web' 
+    },
+    {
+        id: 2,
+        title: 'Form',
+        img: 'img/form.jpg',
+        alt: 'form',
+        link: 'https://andres-webdev.github.io/simple-form/',
+        category: 'movil' 
+    },
+    {
+        id: 3,
+        title: 'Wikipedia Api',
+        img: 'img/wikipedia.jpg',
+        alt: 'wikipedia',
+        link: 'https://andres-webdev.github.io/wikipedia-search-api/',
+        category: 'web' 
+    },
+    {
+        id: 4,
+        title: 'Tasks',
+        img: 'img/clock.jpg',
+        alt: 'tasks',
+        link: 'https://andres-webdev.github.io/tasks/',
+        category: 'movil' 
+    },
+    {
+        id: 5,
+        title: 'Calculator',
+        img: 'img/tasks.jpg',
+        alt: 'calculator',
+        link: 'https://andres-webdev.github.io/calculator/',
+        category: 'web' 
+    },
+    {
+        id: 6,
+        title: 'Counter Words',
+        img: 'img/search.jpg',
+        alt: 'words',
+        link: 'https://andres-webdev.github.io/words-counter/',
+        category: 'movil' 
+    },
+    
+];
+
+export default projects;
