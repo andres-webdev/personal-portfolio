35f5c900d3facefa2dac8237bae51230bd76077d
Andres Marquez <andresdmf55@gmail.com>
2022-04-28 16:14:32 -0500

    Adding Certificates field

--- src/components/SectionSkills.js
+++ src/components/SectionSkills.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import '../stylesheets/sectionSkills.css';
-import { SiJavascript, SiAdobephotoshop, SiAdobeillustrator, SiAdobepremierepro, SiWordpress } from 'react-icons/si';
-import { BsPatchCheckFill, BsGithub, BsGoogle } from 'react-icons/bs';
-import { FaReact, FaGitAlt, FaFacebookF } from 'react-icons/fa';
+import { SiJavascript } from 'react-icons/si';
+import { BsPatchCheckFill, BsGithub } from 'react-icons/bs';
+import { FaReact, FaGitAlt } from 'react-icons/fa';
 import { IoLogoCss3, IoLogoHtml5 } from 'react-icons/io';
+import { RiBookMarkFill } from 'react-icons/ri';
+import { MdOutlineMenuBook } from 'react-icons/md';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -15,7 +17,7 @@ function SectionSkills(){
 
     return(
         <section className="skills section" id="skills">
-            <span className="section-subtitle" data-aos='fade-in'>Competence</span>
+            <span className="section-subtitle" data-aos='fade-in'>Certificates</span>
             <h2 className="section-title" data-aos='fade-in'>Skills</h2>
 
             <div className="skills-container container grid">
@@ -103,21 +105,63 @@ function SectionSkills(){
                     </div>
                 </div>
 
-                <div className="skills-content" data-aos='zoom-in'>
-                    <h3 className="skills-title">Extra Tools</h3>
+                <div className="skills-content" data-aos='zoom-in' data-aos-delay='200'>
+                    <h3 className="skills-title">Certificates</h3>
                 
                     <div className="skills-box">
                         <div className="skills-group">
-                            <div className="skills-data2">
-                                <SiAdobephotoshop className="skills-icons" />
-                                <SiAdobeillustrator className="skills-icons" />
-                                <SiAdobepremierepro className="skills-icons" />
+                            <div className="skills-data" data-aos='fade-in' data-aos-delay='1600'>
+                                <MdOutlineMenuBook className="skills-icons" />
+                                <div className="skills-block3">
+                                    <span className="skills-level">Course</span>
+                                    <h3 className="skills-name">Git and GitHub</h3>
+                                    <span className="skills-level">Platzi - September 2021</span>
+                                </div>
+                            </div>
+                            
+                            <div className="skills-data" data-aos='fade-in' data-aos-delay='1200'>
+                                <MdOutlineMenuBook className="skills-icons" />
+                                <div className="skills-block3">
+                                    <span className="skills-level">Course</span>
+                                    <h3 className="skills-name">Algorithms and Logical Thinking</h3>
+                                    <span className="skills-level">Platzi - July 2021</span>
+                                </div>
                             </div>
 
-                            <div className="skills-data2">
-                                <BsGoogle className="skills-icons" />
-                                <FaFacebookF className="skills-icons" />
-                                <SiWordpress className="skills-icons" />
+                            <div className="skills-data" data-aos='fade-in' data-aos-delay='1000'>
+                                <MdOutlineMenuBook className="skills-icons" />
+                                <div className="skills-block3">
+                                    <span className="skills-level">Course</span>
+                                    <h3 className="skills-name">Digital Product Illustration</h3>
+                                    <span className="skills-level">Platzi - July 2021</span>
+                                </div>
+                            </div>
+
+                            <div className="skills-data" data-aos='fade-in' data-aos-delay='800'>
+                                <MdOutlineMenuBook className="skills-icons" />
+                                <div className="skills-block3">
+                                    <span className="skills-level">Course</span>
+                                    <h3 className="skills-name">Fundamentals of Software Engineering</h3>
+                                    <span className="skills-level">Platzi - August 2021</span>
+                                </div>
+                            </div>
+
+                            <div className="skills-data" data-aos='fade-in' data-aos-delay='1400'>
+                                <MdOutlineMenuBook className="skills-icons" />
+                                <div className="skills-block3">
+                                    <span className="skills-level">Course</span>
+                                    <h3 className="skills-name">Basic Programming</h3>
+                                    <span className="skills-level">Platzi - August 2021</span>
+                                </div>
+                            </div>
+
+                            <div className="skills-data" data-aos='fade-in' data-aos-delay='1800'>
+                                <RiBookMarkFill className="skills-icons" />
+                                <div className="skills-block3">
+                                    <span className="skills-level">Diploma</span>
+                                    <h3 className="skills-name">Bachelor of Technology</h3>
+                                    <span className="skills-level">Universidad Nacional Experimental Politécnica 'Antonio José de Sucre' - June 2021</span>
+                                </div>
                             </div>
                         </div>
                     </div>
