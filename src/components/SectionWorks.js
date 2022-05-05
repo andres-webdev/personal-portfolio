7bdaf4baf144b1d22593e3e530bea34caa9899bc
Andres Marquez <andresdmf55@gmail.com>
2022-05-05 17:34:42 -0500

    Change structure
    Add work-modal-content to display extra details about my projects

--- src/components/SectionWorks.js
+++ src/components/SectionWorks.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import '../stylesheets/sectionWorks.css';
 import projects from './projects';
 import { BiRightArrowAlt } from 'react-icons/bi';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { BsPatchCheckFill } from 'react-icons/bs';
+import { RiPagesFill } from 'react-icons/ri';
+import { BsGithub } from 'react-icons/bs';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -11,9 +15,15 @@ function SectionWork(){
     
     const [ items, setItems ] = useState(projects);
 
+    const [ show, setShow ] = useState(null);
+
     useEffect(() => {
-        AOS.init({ duration: 1800, delay: 500, easing: "ease", once: false });
+        AOS.init({ duration: 1000, easing: "ease", once: false });
     }, []);
+
+    function showDetails(msg){
+        setShow(msg);
+    }
 
     const handlerFilter = (elem) => {
         
@@ -34,30 +44,80 @@ function SectionWork(){
 
     return(
             <section className='work section' id='projects'>
-                <span className='section-subtitle' data-aos='fade-in'>Portfolio</span>
+                <span className='section-subtitle'>Portfolio</span>
                 <h2 className='section-title' data-aos='fade-in'>Recents Projects</h2>
 
-                <div className='work-filters' data-aos='fade-left' data-aos-delay='600'>
+                <div className='work-filters'>
                     <span onClick={() => handlerFilter('all')} className={`work-item ${classChange === 'all' ? 'active-work' : ''}`}>All</span>
                     <span onClick={() => handlerFilter('web')} className={`work-item ${classChange === 'web' ? 'active-work' : ''}`}>Web</span>
                     <span onClick={() => handlerFilter('movil')} className={`work-item ${classChange === 'movil' ? 'active-work' : ''}`}>Movil</span>
                 </div>
 
-                <div className='work-container container grid' data-aos='fade-up' data-aos-delay='800'>
+                <div className='work-container container grid'>
                       
                     {
                         items.map((elem) => {
                         
-                            const { id, title, img, alt, link } = elem;
+                            const { id, title, img, alt, link, description, linkGithub, goal1, goal2, goal3, goal4 } = elem;
 
                             return(
 
                                 <div className='work-card' key={id}>
-                                    <img src={img} alt={alt} className='work-img' />
-                                    <h3 className='work-title'>{title}</h3>
-                                    <a href={link} target="_blank" rel="noreferrer" className='work-button'>
-                                        Demo <BiRightArrowAlt className='work-icon' />
-                                    </a>
+                                    <div className='work-card-item' onClick={() => showDetails(title)}>
+                                        <img src={img} alt={alt} className='work-img' />
+                                        <h3 className='work-title'>{title}</h3>
+                                        <span className="work-button">
+                                            Show more <BiRightArrowAlt className="work-icon" />
+                                        </span>
+                                    </div>
+
+                                    <div className={`work-modal ${show === title ? 'active-modal' : ''}`}>
+                                        <div className="work-modal-content">
+
+                                            <AiOutlineCloseCircle className="work-modal-close" onClick={showDetails} />
+
+                                            <h3 className="work-modal-title">{title}</h3>
+
+                                            <p className="work-modal-description">{description}</p>
+
+                                            <ul className='work-modal-list'>
+                                                <li>
+                                                    <h3 className="work-modal-title2">App Performance</h3>
+                                                    <ul>
+                                                        <li className="work-modal-item">
+                                                            <BsPatchCheckFill className="work-modal-icon" />
+                                                            <p className="work-modal-info">{goal1}</p>
+                                                        </li>
+                                                        <li className="work-modal-item">
+                                                            <BsPatchCheckFill className="work-modal-icon" />
+                                                            <p className="work-modal-info">{goal2}</p>
+                                                        </li>
+                                                        <li className="work-modal-item">
+                                                            <BsPatchCheckFill className="work-modal-icon" />
+                                                            <p className="work-modal-info">{goal3}</p>
+                                                        </li>
+                                                        <li className="work-modal-item">
+                                                            <BsPatchCheckFill className="work-modal-icon" />
+                                                            <p className="work-modal-info">{goal4}</p>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+
+                                            <h4 className='work-modal-links'>Links</h4>
+
+                                            <div className='work-modal-footer'>
+                                                <a href={linkGithub} target="_blank" rel="noreferrer" className='work-button'>
+                                                    <BsGithub className='work-modal-link-icon' />
+                                                </a>
+
+                                                <a href={link} target="_blank" rel="noreferrer" className='work-button'>
+                                                    <RiPagesFill className='work-modal-link-icon' />
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+
                                 </div>
                             )
                         })
