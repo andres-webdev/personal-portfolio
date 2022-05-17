64cd4fa1ec209c7ed513cb70106d1cbbfa4d2c0e
Andres Marquez <andresdmf55@gmail.com>
2022-05-17 11:56:34 -0500

    Correctind Details

--- src/components/projects.js
+++ src/components/projects.js
@@ -10,17 +10,17 @@ const projects = [
         alt: 'binance-api',
         link: 'https://andres-webdev.github.io/binance-api/',
         category: 'web',
-        description: `This app shows a list of cryptocurrencies pairs(USDT) from Binance platform 
-        with its actual market price. When It does the request to the Binance API, updating every 5 seconds, it sorts 
-        data from highest to lowest price, with a sort method. Besides, It calls each element of the array, with
-        a map method, mounting every component with its props. Then, It evaluates, with RegExp, every component with
-        pair USDT and higher than 1 USDT, to represent a row in the list and show the result.`,
+        description: `This app shows a list of cryptocurrencies pairs(USDT) from Binance platform with its actual 
+        market price. When It does a request to the Binance API, it sorts data from highest to lowest price, 
+        with a sort method. Besides, It calls each element of the array, with a map method, mounting every 
+        component with its props. Then, It evaluates, with RegExp, every component with pair USDT and higher 
+        than 1 USDT, to represent a row in the list and show the result.`,
         linkGithub: 'https://github.com/andres-webdev/binance-api',
         goals: [
             `Node.js and NPM for JavaScript runtime environment and package management.`,
             `React.js as a Front-End library to build interactive user interfaces. I used state Hooks to store data
             from the api and handle conection errors.`,
-            `Axios library to handle asynchronous RESTAPI requests using useEffect Hook.`,
+            `Axios library to handle asynchronous REST API requests using useEffect Hook.`,
             `CSS3 for styling and HTML5 for the layout.`
         ]
     },
@@ -31,10 +31,10 @@ const projects = [
         alt: 'form',
         link: 'https://andres-webdev.github.io/simple-form/',
         category: 'movil',
-        description: `This app shows a form with 4 input fields. It evaluates each input field, if all are correct 
-        it shows a green border in other cases are red with a corresponding issue message. Finally, it shows an 
-        alert message with all you data submitted. I used arrow functions, ternary operators, RegExp, validations
-        and render props to share and validate value props between components.`,
+        description: `This app shows a form with 4 input fields. It evaluates each input field, if all are correct it 
+        shows a green border in other cases are red, with a corresponding issue message. Finally, it shows an alert 
+        message with all your data submitted. I used arrow functions, ternary operators, RegExp, validations and 
+        render props to share and validate value props between components.`,
         linkGithub: 'https://github.com/andres-webdev/simple-form',
         goals: [
             `Node.js and NPM for JavaScript runtime environment and package management.`,
@@ -50,17 +50,17 @@ const projects = [
         alt: 'wikipedia',
         link: 'https://andres-webdev.github.io/wikipedia-search-api/',
         category: 'web',
-        description: `This app shows a definition, from Wikipedia website of every word get into searchbar. 
-        When It does the request to Wikipedia API, and gets an array of objects. It calls each element of the 
-        array, with a map method, for mounting a component to show the list. I used render props that allow me
-        share props between components. I used arrow functions and RegExp to evaluate and sort every element 
-        of the list, showing a row in the list with a link and a snippet of the article.`,
+        description: `This app shows a definition, from the Wikipedia website, of every word that gets into the 
+        search bar. When It does a request to the Wikipedia API, it gets an array of objects. It calls each 
+        element of the array, with a map method, for mounting a component to show the list. I used render 
+        props that allowed me to share props between components. I used arrow functions and RegExp to evaluate 
+        and sort every element of the list, showing a row in the list with a link and a snippet of the article.`,
         linkGithub: 'https://github.com/andres-webdev/wikipedia-search-api',
         goals: [
             `Node.js and NPM for JavaScript runtime environment and package management.`,
             `React.js as a Front-End library to build interactive user interfaces. I used state Hooks to store data
             from the api and handle conection errors.`,
-            `Axios library to handle asynchronous RESTAPI requests using useEffect Hook.`,
+            `Axios library to handle asynchronous REST API requests using useEffect Hook.`,
             `CSS3 for styling and HTML5 for the layout.`
         ]
     },
@@ -71,9 +71,9 @@ const projects = [
         alt: 'words',
         link: 'https://andres-webdev.github.io/words-counter/',
         category: 'movil',
-        description: `This app counts every character and word input by user. And It shows the number of characters and 
-        words in the section below. I used render props that allow me share props between components. Also, I used 
-        arrow functions, ternary operators and RexExp.`,
+        description: `This app counts every character and word input by user. And It shows the number of characters 
+        and words in the section below. I used render props that allowed me to share props between components. 
+        Also, I used arrow functions, ternary operators and RexExp.`,
         linkGithub: 'https://github.com/andres-webdev/words-counter',
         goals: [
             `Node.js and NPM for JavaScript runtime environment and package management.`,
@@ -89,10 +89,11 @@ const projects = [
         alt: 'tasks',
         link: 'https://andres-webdev.github.io/tasks/',
         category: 'movil',
-        description: `This app allows you add, mark as completed and delete tasks inside a list. It calls each 
-        element of the array, with a map method, for mounting a component to show the list. I used render props 
-        that allow me share props between components. Also, I used arrow functions, ternary operators and rest 
-        parameters syntax that allows a function to accept an indefinite number of arguments as an array.`,
+        description: `This app allows you to add, mark as completed and delete tasks inside a list. It calls each 
+        element of the array of tasks inputted, with a map method, for mounting a component to show the list. 
+        I used render props that allowed me to share props between components. Also, I used arrow functions, ternary 
+        operators and rest parameters syntax that allows a function to accept an indefinite number of arguments 
+        as an array.`,
         linkGithub: `https://github.com/andres-webdev/tasks`,
         goals: [
             `Node.js and NPM for JavaScript runtime environment and package management.`,
